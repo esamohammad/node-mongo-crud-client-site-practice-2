5e516f8b6b958c5cff761726b0c6b00c896c4c78
mdesabd <esa.ce2020@gmail.com>
2023-01-28 02:00:29 +0600

    raed/get all data at a time

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+   const users = useLoaderData();
    return (
       <div>
-         Home
+         <h2>Total User:- {users.length}</h2>
+         {
+            users.map(user=><p key={user._id}>{user.name} {user.roll} {user.email}</p>)
+         }
       </div>
    );
 };
