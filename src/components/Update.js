11756451de4d5ed91a0446d33a22becf907528b7
mdesabd <esa.ce2020@gmail.com>
2023-01-28 22:39:30 +0600

    update value show on server

--- src/components/Update.js
+++ src/components/Update.js
@@ -6,35 +6,47 @@ const Update = () => {
    const storedUser = useLoaderData();
    const [user, setUser] = useState(storedUser);
 
-   const handleUpdateUser = event =>{
+   const handleUpdateUser = event => {
       event.preventDefault();
       console.log(user);
-      
-      
-  }
 
-   const handleInputChange = event =>{
-        const field = event.target.name;
-        const value = event.target.value;
-        const newUser = {...user}
-        newUser[field] = value;
-        setUser(newUser);
-    }
+      //data pathate hobe na hole server pabe kivabe--
+      fetch(`http://localhost:5000/users/${storedUser._id}`, {
+         method: 'PUT',
+         headers: {
+            'content-type': 'application/json'
+         },
+         body: JSON.stringify(user)
+      })
+         .then(res => res.json())
+         .then(data => {
+            console.log(data);
+         })
 
-    return (
-        <div>
-            <h2>Please Update: {storedUser.name}</h2>
-            <form onSubmit={handleUpdateUser}>
-                <input onChange={handleInputChange} defaultValue={storedUser.name} type="text" name='name' placeholder='name' required />
-                <br />
-                <input onChange={handleInputChange} type="text" defaultValue={storedUser.address} name='address' placeholder='address' required />
-                <br />
-                <input onChange={handleInputChange} type="email" defaultValue={storedUser.email} name="email" id="" placeholder='email' required />
-                <br />
-                <button type="submit">Update User</button>
-            </form>
-        </div>
-    );
+   }
+
+   const handleInputChange = event => {
+      const field = event.target.name;
+      const value = event.target.value;
+      const newUser = { ...user }
+      newUser[field] = value;
+      setUser(newUser);
+   }
+
+   return (
+      <div>
+         <h2>Please Update: {storedUser.name}</h2>
+         <form onSubmit={handleUpdateUser}>
+            <input onChange={handleInputChange} defaultValue={storedUser.name} type="text" name='name' placeholder='name' required />
+            <br />
+            <input onChange={handleInputChange} type="text" defaultValue={storedUser.address} name='address' placeholder='address' required />
+            <br />
+            <input onChange={handleInputChange} type="email" defaultValue={storedUser.email} name="email" id="" placeholder='email' required />
+            <br />
+            <button type="submit">Update User</button>
+         </form>
+      </div>
+   );
 };
 
 export default Update;
