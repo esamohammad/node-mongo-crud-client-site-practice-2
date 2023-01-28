a40fdb3ff8eea0d7e75e6bfcab3295544d47465c
mdesabd <esa.ce2020@gmail.com>
2023-01-28 21:29:05 +0600

    update defult value set auto ..value show on consol

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
-   
-   const user = useLoaderData();
 
+   const storedUser = useLoaderData();
+   const [user, setUser] = useState(storedUser);
 
-   return (
-      <div>
-         <h2>Please Update: {user.name}</h2>
-      </div>
-   );
+   const handleUpdateUser = event =>{
+      event.preventDefault();
+      console.log(user);
+      
+      
+  }
+
+   const handleInputChange = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+    return (
+        <div>
+            <h2>Please Update: {storedUser.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input onChange={handleInputChange} defaultValue={storedUser.name} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onChange={handleInputChange} type="text" defaultValue={storedUser.address} name='address' placeholder='address' required />
+                <br />
+                <input onChange={handleInputChange} type="email" defaultValue={storedUser.email} name="email" id="" placeholder='email' required />
+                <br />
+                <button type="submit">Update User</button>
+            </form>
+        </div>
+    );
 };
 
 export default Update;
